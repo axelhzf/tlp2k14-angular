01aa2ef2073a62b69d4c1fe785e2f5900a0ea873
Axel Hernández Ferrera <axelhzf@gmail.com>
2014-07-13 18:42:37 +0100

    base

--- base/app/client/js/socket.js
+++ base/app/client/js/socket.js
@@ -1,4 +1,8 @@
 app.factory("socket", function ($location) {
+
+//  CONEXIÓN CON EL SERVIDOR
+//  -------------------------
+
 //  var socket = io();
 //
 //  socket.bindToScope = function ($scope) {
@@ -16,59 +20,72 @@ app.factory("socket", function ($location) {
 //  };
 //
 //  return socket;
+
+
+// TESTING EN LOCAL
+
+  var socket = {};
+
+  socket.bindToScope = function ($scope) {
+    $scope.state = "tournament:name";
+    $scope.tournament = {
+      name: "Torneo de la muerte",
+      maxUsers: 4,
+      users: [
+        {name: "Pepe"},
+        {name: "Juan"}
+      ],
+      brackets: [
+        [
+          [
+            {name: "Pepe", id: 1},
+            {name: "Juan", id: 2}
+          ],
+          [
+            {name: "Pepe", id: 3},
+            {name: "Juan", id: 4}
+          ]
+        ],
+        [
+          [
+            {name: "", id: -1},
+            {name: "", id: -1}
+          ]
+        ],
+        [
+          [
+            {name: ""}
+          ]
+        ]
+      ]
+    }
+    $scope.user = {
+      id: 1,
+      name: "Axel"
+    };
+    $scope.activeMatch = {
+      user1: {
+        id: 1, name: "Axel"
+      },
+      user2: {
+        id: 2, name: "Andrea"
+      },
+      play1: "rock",
+      play2: "spock",
+      winner: {
+        name: "Axel"
+      }
+    };
+    $scope.winner = "PEPE";
+
+
+    var path = $scope.state.replace(/:/, "/");
+    $location.path(path).replace();
+
+  };
+  return socket;
+
 });
 
 
 
-//    setToScope({
-//      state: "tournament:chart",
-//      tournament: {
-//        name: "Torneo de la muerte",
-//        maxUsers: 4,
-//        users: [
-//          {name: "Pepe"},
-//          {name: "Juan"}
-//        ],
-//        brackets: [
-//          [
-//            [
-//              {name: "Pepe", id: 1},
-//              {name: "Juan", id: 2}
-//            ],
-//            [
-//              {name: "Pepe", id: 3},
-//              {name: "Juan", id: 4}
-//            ]
-//          ],
-//          [
-//            [
-//              {name: "", id: -1},
-//              {name: "", id: -1}
-//            ]
-//          ],
-//          [
-//            [
-//              {name: ""}
-//            ]
-//          ]
-//        ]
-//      },
-//      user: {
-//        id: 1,
-//        name: "Axel"
-//      },
-//      activeMatch: {
-//        user1: {
-//          id: 1, name: "Axel"
-//        },
-//        user2: {
-//          id: 2, name: "Andrea"
-//        }
-////    play1: "rock",
-////    play2: "spock",
-////    winner: {
-////      name: "Axel"
-////    }
-//      },
-//      winner: "PEPE"
-//    })
